296a40173ad9e64b2ca86b3d9e5dfcfcca5c648a
Mehdi <mehdi@gmail.com>
2026-07-30 13:47:45 +0330

    edited8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.loveapp_android"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
